31cb3bbdcf35008ec438a29d28d53cb00e64f1b9
Abril Rodriguez Almanzar <155594542+abrodriguezalman@users.noreply.github.com>
2026-03-03 19:59:43 -0600

    finished main page prototype, publishing site to test links

---
+++ main.js
@@ -0,0 +1,23 @@
+/* global nn */
+let data
+
+async function setup () {
+  const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSGgmPzZ3i3fj_ELzRLoxVvBiAPGQwJ-O8IPZR-lBkIiofkffJgj4FtZTE03aYb1EIP_Vp6uxHHrHix/pub?output=csv'
+  data = await nn.loadData(url)
+  data = nn.parseData(data)
+  console.log(data)
+  updateContent()
+}
+
+function updateContent () {
+  const hash = window.location.hash
+  const id = hash.slice(1)
+  if (id) {
+    const item = data.find(i => i.id === id)
+    nn.get('#title').content(item.title)
+    nn.get('#date').content(item.date)
+  }
+}
+
+nn.on('load', setup)
+  
